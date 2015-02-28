abbe58798c62958d8d8a28ef3b23f8fd9e6accd5
andrewmbacon <andrew.bacon@me.com>
2015-02-28 12:43:10 -0500

    Setting up Footer

--- footer.php
+++ footer.php
@@ -7,19 +7,57 @@
  * @package commonplace
  */
 ?>
-
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'commonplace' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'commonplace' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'commonplace' ), 'commonplace', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+</div>
+<!-- #content -->
+<?php if ( is_active_sidebar( 'sidebar-footer' ) ) { ?>
+	<div id="promos">
+		<?php dynamic_sidebar( 'sidebar-footer' ); ?>
+	</div>
+<?php }; ?>
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<?php
+				// Only display the footer if a menu of "Footer" exists in the site. 
+				if (wp_get_nav_menu_object('Footer')){
+					$defaults = array(
+						'menu'			=> 'Footer',
+						'container'       => false,
+						'items_wrap'      => '%3$s',
+						'depth'			=> 1,
+						'fallback_cb'		=> false
+					);
+					wp_nav_menu( $defaults );
+				}
+				?>
+			</ul>
+		</div>
+	</div>
+</nav>
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="site-info">
+	<ul>
+		<li><span id="copyright">&copy; <?php echo date("Y"); ?> Common-place.</span> <span id="issn">ISSN 1544-824X</span></li>
+		<li><a href="<?php echo site_url(); ?>/wp-admin/">Editor Login</a></li>
+		<?php
+		// Only display the footer if a menu of "Footer" exists in the site. 
+		if (wp_get_nav_menu_object('SubFooter')){
+			$defaults = array(
+				'menu'			=> 'SubFooter',
+				'container'       => false,
+				'items_wrap'      => '%3$s',
+				'depth'			=> 1,
+				'fallback_cb'		=> false
+			);
+			wp_nav_menu( $defaults );
+		}
+		?>
+	</ul>
+	<!-- .site-info --> 
+</footer>
+<!-- #colophon -->
+</div>
+<!-- #page -->
 <?php wp_footer(); ?>
-
-</body>
-</html>
+</body></html>
--- functions.php
+++ functions.php
@@ -44,6 +44,15 @@ function gazette_widgets_init() {
 		'before_title'  => '<h1 class="widget-title">',
 		'after_title'   => '</h1>',
 	) );
+	register_sidebar( array(
+		'name'          => __( 'Footer', 'commonplace' ),
+		'id'            => 'sidebar-footer',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 }
 add_action( 'widgets_init', 'gazette_widgets_init' );
 
