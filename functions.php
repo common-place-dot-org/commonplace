88cf93dda19671f493baa47bef5cdb8343b82713
Sam <sam.a.menza@gmail.com>
2015-03-24 08:26:09 -0400

    Fixed functions.php merge

--- functions.php
+++ functions.php
@@ -118,3 +118,5 @@ require get_template_directory() . '/inc/require-post-category.php';
 /*Requires article to have issue picked upon creation
 */
 require get_template_directory() . '/inc/count_features.php';
+
+require get_template_directory() . '/inc/images.php';
