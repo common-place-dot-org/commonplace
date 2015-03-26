e283f1fb55438e203e0e1c856dfdc1c0bc04c06f
andrewmbacon <andrew.bacon@me.com>
2015-03-26 09:29:22 -0400

    Adding layout logic for broadsides

--- page-broadside.php
+++ page-broadside.php
@@ -44,19 +44,33 @@ $roundtables_args = array(
 
 	$features = get_posts( $features_args );
 	$features_count = count($features); 
+	$features_count = 4;
 	
 	$roundtables = get_posts( $roundtables_args );
 	$roundtables_count = count($roundtables); 
+	//$roundtables_count = 0; 
 
 ?>
 <div class="broadside" id="issue-<?php echo $issue_slug; ?>">
 	<div class="row">
 		<?php 
-			// Are there going to be roundtables?
+		
+			/* 
+			*	Layout Variables
+			*	Used to change the bootstrap col numbers based on number of feature articles and roundtable articles. - AB
+			*	
+			*/
+		
+			
 			if ($roundtables_count > 0 ){
+				// YES ROUNDTABLES
+				
+				// change the width of the 'features' wrapper
 				$features_grid = 7;
+				// change feature articles to single column. 
 				$features_articles_grid = 12;
 				
+				// add more or less room for img based on number of articles. 
 				if ($features_count > 3 ){
 					$feature_img_grid = 3;
 					$feature_text_grid = 9;
@@ -64,32 +78,104 @@ $roundtables_args = array(
 					$feature_img_grid = 5;
 					$feature_text_grid = 7;
 				}
-				
-			} else {
-				$features_grid = 12;
+				// change widths and orientation of articles depending on number. 
 				switch ($features_count) {
+					// there are 2 features
 					case 2:	
-						$features_articles_grid = 6;
+						$feature_split = false;
+						$features_articles_grid = 12;
+						$feature_img_grid = 12;
+						$feature_text_grid = 12;
 						break;
-					case 3:	
-						$features_articles_grid = 4;
+					// there are 3 features
+					case 3:		
+						$feature_split = false;
+						$features_articles_grid = 12;
+						$feature_img_grid = 4;
+						$feature_text_grid = 8;
 						break;
+					case 4:	
+						$feature_split = false;
+						$features_articles_grid = 12;
+						$feature_img_grid = 3;
+						$feature_text_grid = 9;
+						break;
+					// if there are 5+ features
+					default :
+						$feature_split = false;
+						$features_articles_grid = 12;
+						$feature_img_grid = 2;
+						$feature_text_grid = 10;
 				}
 				
-			
-					$feature_img_grid = 1;
-					$feature_text_grid = 1;
+			} else {
+				// NO ROUNDTABLES 
+				
+				// set 'features' wrapper to full width. 
+				$features_grid = 12;
+				
+				// change widths and orientation of articles depending on number. 
+				switch ($features_count) {
+					// there are 2 features
+					case 2:	
+						$feature_split = false;
+						$features_articles_grid = 6;
+						$feature_img_grid = 12;
+						$feature_text_grid = 12;
+						break;
+					// there are 3 features
+					case 3:		
+						$feature_split = false;
+						$features_articles_grid = 4;
+						$feature_img_grid = 12;
+						$feature_text_grid = 12;
+						break;
+					case 4:	
+						$feature_split = false;
+						$features_articles_grid = 3;
+						$feature_img_grid = 12;
+						$feature_text_grid = 12;
+						break;
+					// if there are 5+ features
+					default :
+						$feature_split = true;
+						$features_articles_grid = 6;
+						$feature_img_grid = 4;
+						$feature_text_grid = 8;
+				}
+				
+				// width for each element within the article
 				
 			};
 		?> 
+		<?php 
+		/* 
+		*	Generate Features Articles
+		*/
+		?>
 		<div class="col-sm-<?php echo $features_grid;?>" id="features">
 			<?php $query = new WP_Query($features_args); ?>
-			<?php 
+			<?php
+			// used to calculate odds and evens. 
+			$featureCounter = 1; 
 			while ( $query->have_posts() ) {
 				$query->the_post();
+				
+				// Is this an odd article or even 
+				if ($feature_split) {
+					if ($featureCounter % 2 == 0) {
+						$feature_even = true;
+					} else {
+						$feature_odd = true;
+					}
+					// start a row if this is an odd numbered article
+					if ($feature_odd) {
+						echo '<div class="row">';
+					}	
+				}
 				?>
 				<div class="col-sm-<?php echo $features_articles_grid;?>">
-					<article>
+					<article id="article-<?php the_ID(); ?>" <?php post_class(); ?>>
 						<div class="col-sm-<?php echo $feature_img_grid; ?>">
 							<figure class="article-img">
 								<a href="<?php the_permalink();?>"><? the_post_thumbnail('post-thumbnail', array(
@@ -104,11 +190,29 @@ $roundtables_args = array(
 					</article>
 				</div>
 				<?php 
+				
+				if ($feature_split) {
+					// close the row if this is an even numbered article
+					if ($feature_even) {
+						echo '</div>';
+					}
+					// used to calculate odds and evens. 
+					$featureCounter++;
+					$feature_even = false;
+					$feature_odd = false;
+				}
 			}
 			?>
 			<?php wp_reset_postdata(); ?>
 		</div> <!-- /features -->
-		<?php if ($roundtables_count > 0 ){?>
+		<?php 
+		
+		/* 
+		*	Generate Roundtable Articles
+		*/
+		
+		
+		if ($roundtables_count > 0 ){?>
 			
 			<div class="col-sm-5" id="roundtables">
 				<?php	
