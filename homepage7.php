223f418edc3690f93b41df59c644818a25216067
andrewmbacon <andrew.bacon@me.com>
2015-03-29 10:06:35 -0400

    Removing Homepage Files
    This functionality rolled into the page-broadside.php template.

--- homepage7.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-* Template Name: Homepage 7+
-* Description: Homepage for 7+ features
-*/
-
-?>
--- homepage7_round.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-* Template Name: Homepage 7+ Round
-* Description: Homepage for 7+ features with round tables
-*/
-
-?>
