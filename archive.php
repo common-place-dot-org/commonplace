2f22f6bfe8250d9d2b21283770674879bb71fc57
Sam <sam.a.menza@gmail.com>
2015-04-06 07:49:12 -0400

    Changed broadside-top.php to home.php, edited archive.php

--- archive.php
+++ archive.php
@@ -1,105 +1,62 @@
 <?php
 /**
  * The template for displaying archive pages.
- *
+ *Template Name: Archives
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package commonplace
  */
+$args=array(
+			'post_type'=>'article',
+			'post_status'      => 'publish'
+		);
 
-get_header(); ?>
+get_header();?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<?php wp_dropdown_categories( 'hide_empty=0' ); ?>
 
-		<?php if ( have_posts() ) : ?>
+		<section id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
 
-			<header class="page-header">
-				<h1 class="page-title">
+		  <?php if(function_exists('show_beautiful_filters')){ show_beautiful_filters(); }
+			if ( have_posts() ) : ?>
+
+				<header class="page-header">
 					<?php
-						if ( is_category() ) :
-							single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', 'commonplace' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'commonplace' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'commonplace' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'commonplace' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'commonplace' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'commonplace' ) ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'commonplace' );
-
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', 'commonplace' );
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'commonplace' );
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'commonplace' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'commonplace' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'commonplace' );
-
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', 'commonplace' );
-
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', 'commonplace' );
-
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', 'commonplace' );
-
-						else :
-							_e( 'Archives', 'commonplace' );
-
-						endif;
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
 					?>
-				</h1>
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+				</header><!-- .page-header -->
 
 				<?php
-					/* Include the Post-Format-specific template for the content.
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
 					get_template_part( 'content', get_post_format() );
-				?>
 
-			<?php endwhile; ?>
+				// End the loop.
+				endwhile;
 
-			<?php commonplace_paging_nav(); ?>
+				// Previous/next page navigation.
+				the_posts_pagination( array(
+					'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
+					'next_text'          => __( 'Next page', 'twentyfifteen' ),
+					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+				) );
 
-		<?php else : ?>
+			// If no content, include the "No posts found" template.
+			else :
+				get_template_part( 'content', 'none' );
 
-			<?php get_template_part( 'content', 'none' ); ?>
+			endif;
+			?>
 
-		<?php endif; ?>
+			</main><!-- .site-main -->
+		</section><!-- .content-area -->
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+	<?php get_footer(); ?>
--- home.php
+++ home.php
@@ -1,12 +1,16 @@
-<?php 
+<?php
 
 /*
 *Template Name: Home
-* 
-*/ 
+*
+*/
 get_header();
 
 $current_issue=$wpdb->get_var( "SELECT option_value FROM wp_options WHERE option_name='current_issue'");
+
+$extra_issue=$wpdb->get_var( "SELECT option_value FROM wp_options WHERE option_name='extra_issue'");
+
+$issue_id=$wpdb->get_var($wpdb->prepare("SELECT term_id FROM wp_terms WHERE name=%s",$current_issue));
 
 $features_args=array(
 			'post_type'=>'article',
@@ -17,15 +21,17 @@ $features_args=array(
 										'taxonomy'=>'issue',
 										'field'=>'name',
 										'terms'=>$current_issue,
+										'include_children'=>$extra_issue,
 										),
 									array(
 										'taxonomy'=>'column',
 										'field'=>'name',
 										'terms'=>'Features',
+										'include_children'=>$extra_issue,
 									)
 			)
 		);
-		
+
 $features_query = new WP_Query($features_args);
 $features_count = $features_query->found_posts;
 
@@ -38,246 +44,268 @@ $roundtable_args=array(
 									'taxonomy'=>'issue',
 									'field'=>'name',
 									'terms'=>$current_issue,
+									'include_children'=>$extra_issue,
 									),
 								array(
 									'taxonomy'=>'column',
 									'field'=>'name',
 									'terms'=>'Roundtable',
+									'include_children'=>$extra_issue,
 								)
 		)
 	);
-		
+
 $roundtables_query = new WP_Query($roundtable_args);
 $roundtables_count = $roundtables_query->found_posts;
 
-?>
-<div class="container"> 
+//The featured issue picutre
+//Use $issue_image_url to output image url into theme
 
-<?php 
-//If there are <3 features 
-if($features_count<=3){
-	//Round tables are present
-	 if($roundtables_count>0){ ?>
-		<div class="row">
-			<div class="col-sm-6">
-				<section id="features">
-					<h2>Features</h2>
-					<?php
-					$count=0;
-					while ( $features_query->have_posts() ) {
-						if($count<=3){
-						$features_query->the_post();
-						echo '<div class="col-sm-12">';
-						echo '<article>';
-						if ( has_post_thumbnail() ) {
-							the_post_thumbnail(array(150,150));
-						} 
-						echo '<h3 class="article-title">'.get_the_title()."</h3>";
-						echo '<div class="article-excerpt">'.get_the_excerpt()."</div>";
-						echo '</article>';
-						echo '</div>';
-						$count++;
-						}
-						else{
-							$query->the_post();
-						}
-					}
-					wp_reset_postdata();
-					?>
-				</section>
-			</div>
-			<div class="col-sm-6">
-				<section id="features">
-					<h2>Roundtables</h2>
-					<?php
-					$count=0;
-					while ( $roundtables_query->have_posts() ) {
-						$roundtables_query->the_post();
-						if($count==0){
-							echo '<div class="col-sm-12">';
-							echo '<article>';
-							if ( has_post_thumbnail() ) {
-								the_post_thumbnail(array(150,150));
-							} 
-							echo '<h3 class="article-title">'.get_the_title()."</h3>";
-							echo '<div class="article-excerpt">'.get_the_excerpt()."</div>";
-							echo '</article>';
-							echo '</div>';
-							$count++;
-						}
-						else{
-							echo '<div class="col-sm-12">';
-							echo '<article>';
-							echo '<h3 class="article-title">'.get_the_title()."</h3>";
-							//Use this line to get the author// echo '<div class="article-excerpt">'.get_the_excerpt()."</div>";
-							echo '</article>';
-							echo '</div>';
-							$count++;
-						}
-					}
-					wp_reset_postdata();
-					?>
-				</section>
-			</div>
-		</div>
-		<?php
+if (function_exists('z_taxonomy_image_url')){
+		$issue_image_url=z_taxonomy_image_url($issue_id);
 	}
-	else{ ?>
-			<div class="row">
-			<section id="features">
-			<h2>Features</h2>
+	else{
+		echo "<h1>false</h1>";
+	}
+?>
+
+<!-- Sample code for current issue image -->
+<div class-"span12">
+			<img src="<?php echo $issue_image_url;?>" alt="featured image">
+</div>
+
+
+<?php
+
+// Layout variables: Determined by the Article count.
+
+// if this issue has any round table articles...
+if ($roundtables_count > 0 ){
+
+	// change the width of the 'features' wrapper
+	$features_grid = 7;
+	// change feature articles to single column.
+	$features_articles_grid = 12;
+
+	// add more or less room for img based on number of articles.
+	if ($features_count > 3 ){
+		$feature_img_grid = 3;
+		$feature_text_grid = 9;
+	} else {
+		$feature_img_grid = 5;
+		$feature_text_grid = 7;
+	}
+	// change widths and orientation of articles depending on number.
+	switch ($features_count) {
+		// there are 2 features
+		case 2:
+			$feature_split = false;
+			$features_articles_grid = 12;
+			$feature_img_grid = 12;
+			$feature_text_grid = 12;
+			break;
+		// there are 3 features
+		case 3:
+			$feature_split = false;
+			$features_articles_grid = 12;
+			$feature_img_grid = 4;
+			$feature_text_grid = 8;
+			break;
+		case 4:
+			$feature_split = false;
+			$features_articles_grid = 12;
+			$feature_img_grid = 3;
+			$feature_text_grid = 9;
+			break;
+		// if there are any other number of features
+		default :
+			$feature_split = false;
+			$features_articles_grid = 12;
+			$feature_img_grid = 2;
+			$feature_text_grid = 10;
+	}
+
+} else {
+
+	// If this issue does not have a roundtable....
+
+	// set 'features' wrapper to full width.
+	$features_grid = 12;
+
+	// change widths and orientation of articles depending on number.
+	switch ($features_count) {
+		// there are 2 features
+		case 2:
+			$feature_split = false;
+			$features_articles_grid = 6;
+			$feature_img_grid = 12;
+			$feature_text_grid = 12;
+			break;
+		// there are 3 features
+		case 3:
+			$feature_split = false;
+			$features_articles_grid = 4;
+			$feature_img_grid = 12;
+			$feature_text_grid = 12;
+			break;
+		// there are 4 features
+		case 4:
+			$feature_split = false;
+			$features_articles_grid = 3;
+			$feature_img_grid = 12;
+			$feature_text_grid = 12;
+			break;
+		// if there are any other number of features
+		default :
+			$feature_split = true;
+			$features_articles_grid = 6;
+			$feature_img_grid = 4;
+			$feature_text_grid = 8;
+	}
+
+	// width for each element within the article
+
+};
+?>
+
+
+<div class="row" id="main-top">
+		<div class="col-sm-<?php echo $features_grid;?>" id="features">
+			<header>
+				<h2>Features</h2>
+			</header>
 			<?php
-			$count=0;
-			while ( $features_query->have_posts() ) {
-				if($count<=3){
-				$features_query->the_post();
-				echo '<div class="col-sm-4">';
-				echo '<article>';
-				if ( has_post_thumbnail() ) {
-					the_post_thumbnail(array(150,150));
-				} 
-				echo '<h3 class="article-title">'.get_the_title()."</h3>";
-				echo '<div class="article-excerpt">'.get_the_excerpt()."</div>";
-				echo '</article>';
-				echo '</div>';
-				$count++;
+			// query the features.
+			$query = new WP_Query($features_args);
+
+
+			// used to calculate odds and evens.
+			$featureCounter = 1;
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				// are there so many features that we need 2 columns of them?
+				if ($feature_split) {
+					// in 2 col mode, we need to track even and odd articles to create rows.
+					if ($featureCounter % 2 == 0) {
+						$feature_even = true;
+					} else {
+						$feature_odd = true;
+					}
+					// start a row if this is an odd numbered article
+					if ($feature_odd) {
+						echo '<div class="row">';
+					}
+				} else {
+					echo '<div class="row">';
 				}
-				else{
-					$query->the_post();
+				?>
+				<div class="col-sm-<?php echo $features_articles_grid;?>">
+					<article id="article-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<div class="row">
+							<div class="col-sm-<?php echo $feature_img_grid; ?>">
+								<figure class="article-img">
+									<a href="<?php the_permalink();?>"><? if ( has_post_thumbnail() ) {
+												the_post_thumbnail('post-thumbnail', array(
+												'class' => "attachment-$size img-responsive",
+												));
+											};?></a>
+								</figure>
+							</div>
+							<div class="col-sm-<?php echo $feature_text_grid ?>">
+								<h3 class="article-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+								<p class="article-description"><?php echo category_description( $category_id ); ?></p>
+								<div class="article-excerpt"><?php the_excerpt();?></div>
+							</div>
+						</div>
+					</article>
+				</div>
+				<?php
+				// if we're running 2 cols, we don;t always want to close the row.
+				if ($feature_split) {
+					// close the row if this is an even numbered article, or the last article in the bunch.
+					if ($feature_even || ($featureCounter == $features_count)) {
+						echo '</div>';
+					}
+					//increase the article counter
+					$featureCounter++;
+					// reset odd/even.
+					$feature_even = false;
+					$feature_odd = false;
+				} else {
+					// if we're not in 2col mode, always close the row.
+					echo '</div>';
 				}
 			}
+			?>
+			<?php
+			//reset the query
 			wp_reset_postdata();
 			?>
-			</section>
-		</div>
-	
-	<?php
-  }
-}
-else if(3<$features_count || $features_count<=6){
-  if($roundtables_count>0){ ?>
-    <div class="row">
-		<div class="col-sm-8">
-			<section id="features">
-				<h2>Features</h2>
-				<?php
-				$count=0;
-				echo "<div class='col-sm-6'>";
-				while ( $features_query->have_posts() ) {
-					if($count<=3){
-					$features_query->the_post();
-					echo '<div class="col-sm-6">';
-					echo '<article>';
-					if ( has_post_thumbnail() ) {
-						the_post_thumbnail(array(150,150));
-					} 
-					echo '<h3 class="article-title">'.get_the_title()."</h3>";
-					echo '<div class="article-excerpt">'.get_the_excerpt()."</div>";
-					echo '</article>';
-					echo '</div>';
-					$count++;
-					}
-					else{
-						$query->the_post();
-					}
-				}
-				echo "</div>";
-				$count=0;
-				echo "<div class='col-sm-6'>";
-				while ( $features_query->have_posts() ) {
-					if($count>3){
-					$features_query->the_post();
-					echo '<div class="col-sm-4">';
-					echo '<article>';
-					if ( has_post_thumbnail() ) {
-						the_post_thumbnail(array(150,150));
-					} 
-					echo '<h3 class="article-title">'.get_the_title()."</h3>";
-					echo '<div class="article-excerpt">'.get_the_excerpt()."</div>";
-					echo '</article>';
-					echo '</div>';
-					$count++;
-					}
-					else{
-						$query->the_post();
-					}
-				}
-				echo "</div>";
-				wp_reset_postdata();
-				?>
-			</section>
-		</div>
-		<div class="col-sm-4">
-			<section id="features">
-				<h2>Roundtables</h2>
-				<?php
-				$count=0;
-				while ( $roundtables_query->have_posts() ) {
-					$roundtables_query->the_post();
-					if($count==0){
-						echo '<div class="col-sm-12">';
-						echo '<article>';
-						if ( has_post_thumbnail() ) {
-							the_post_thumbnail(array(150,150));
-						} 
-						echo '<h3 class="article-title">'.get_the_title()."</h3>";
-						echo '<div class="article-excerpt">'.get_the_excerpt()."</div>";
-						echo '</article>';
-						echo '</div>';
-						$count++;
-					}
-					else{
-						echo '<div class="col-sm-12">';
-						echo '<article>';
-						echo '<h3 class="article-title">'.get_the_title()."</h3>";
-						//Use this line to get the author// echo '<div class="article-excerpt">'.get_the_excerpt()."</div>";
-						echo '</article>';
-						echo '</div>';
-						$count++;
-					}
-				}
-				wp_reset_postdata();
-				?>
-			</section>
-		</div>
-	</div>
-	
-	<?php
-  }
-  else{ ?>
-    <div class="row">
-		<section id="features">
-		<h2>Features</h2>
+		</div> <!-- /features -->
 		<?php
-		$count=0;
-		while ( $features_query->have_posts() ) {
-			$features_query->the_post();
-			echo '<div class="col-sm-6">';
-			echo '<article>';
-			if ( has_post_thumbnail() ) {
-				the_post_thumbnail(array(150,150));
-			} 
-			echo '<h3 class="article-title">'.get_the_title()."</h3>";
-			echo '<div class="article-excerpt">'.get_the_excerpt()."</div>";
-			echo '</article>';
-			echo '</div>';
-			$count++;
-			}
-		wp_reset_postdata();
-		?>
-		</section>
-	</div>
-	<?php
-  }
-}
-  else{
-    if($roundtables_count>0){
-      echo "featured >7 with round";
-    }
-    else{
-      echo "featured >7";
-    }
-  }?>
-  </div> <!-- End of Container -->
-  
-  <?php get_footer();?>
+		// If we need a roundtables section...
+		if ($roundtables_count > 0 ){?>
+
+			<div class="col-sm-5" id="roundtables">
+				<?php
+				// using this to track the first roundtable article.
+				$roundtableCounter = 0;
+				$query2 = new WP_Query($roundtables_args);
+				while ( $query2->have_posts() ) {
+					$query2->the_post();
+					?>
+					<?php
+
+					// Roundtable header
+
+					// Create a roundtable header from column and feat. img of first article.
+					if ($roundtableCounter == 0) {?>
+						<header>
+							<?php
+							if (get_the_post_thumbnail() != '') {
+								// if there's a featured image, display that. ?>
+								<div class="roundtable-img">
+									<? if ( has_post_thumbnail() ) {
+												the_post_thumbnail('post-thumbnail', array(
+												'class' => "attachment-$size img-responsive",
+												));
+										 };?>
+								</div><!-- /roundtable-img -->
+							<?php
+							} else {
+								// if not, let's display the name of the roundtable in text.
+								$terms = get_the_terms($post->ID,'column');
+								foreach ($terms as $term) {
+									// Don't print the info of the parent column "roundtables" just the info for the child column, the specfici roundtable.
+									$parent = "$term->parent";
+									if ($parent != 0) {
+										echo '<h2 class="roundtable-description">';
+											echo "$term->description";
+										echo '</h2>';
+									}
+								}
+							}
+							?>
+						</header>
+					<?php
+					}
+
+					// Roundtable articles.
+
+					?>
+					<article>
+						<h3 class="article-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+						<p class="article-description"><?php echo category_description( $category_id ); ?></p>
+						<div class="article-excerpt"><?php the_excerpt();?></div>
+					</article>
+					<?php $roundtableCounter++; ?>
+				<?php
+				} //endwhile
+				?>
+			</div><!-- /roundtables -->
+		<?php
+		} else {
+			echo 'roundtables is <= 0';
+		}
+		get_footer();?>
+	</div><!--  /main-top -->
