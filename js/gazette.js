0138df2bc59899b02c5272357ad1a61928d11862
andrewmbacon <andrew.bacon@me.com>
2015-05-16 13:52:29 -0400

    Reformat search form, cleaning up JS file.

--- js/gazette.js
+++ js/gazette.js
@@ -1,6 +1,15 @@
-// JavaScript Document
-jQuery(document).ready(function() {
-jQuery("#searchsubmit").attr("value", "Submit");
-console.log("test");
+jQuery(document).ready(function($) {
+// sam.. why is this here? commented out. - ab 
+//jQuery("#searchsubmit").attr("value", "Submit");
 
+
+	function bsearchformBootstrapper(){
+		$('.bsearchform').each(function(){
+			$(this).children('.s').addClass('form-control');
+			$(this).children('.searchsubmit').addClass('btn btn-primary');
+			$(this).addClass('form-group').addClass('form-inline');
+		});	
+	}
+	
+	bsearchformBootstrapper()
 });
