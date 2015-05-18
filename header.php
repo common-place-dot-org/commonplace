728ea00f49117589966f00eecf93bb90766c7092
andrewmbacon <andrew.bacon@me.com>
2015-05-18 19:59:35 -0400

    Adding favicon & IE8 Shims

--- header.php
+++ header.php
@@ -10,11 +10,18 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory')?>/favicon.ico" />
 <title><?php wp_title('›', true, 'right'); ?>Common-place: The Journal of early American Life</title>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link type="text/plain" rel="author" href="<?php echo esc_url( home_url( '/' ) ); ?>/humans.txt" />
 <?php wp_head(); ?>
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body <?php body_class(); ?>>
 	<nav id="skiplinks" class="sr-only">
