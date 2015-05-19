55d22d156e661f7873e021a5e71a50dc6e5e0a90
andrewmbacon <andrew.bacon@me.com>
2015-05-18 20:00:58 -0400

    added page-wide template

---
+++ page-wide.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Blank (no styles)
+*/
+get_header(); ?>
+
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				
+				<?php the_content(); ?>
+				
+
+			<?php endwhile; // end of the loop. ?>
+
+<?php get_footer(); ?>
