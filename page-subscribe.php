45cb9341e9e1abf999776279e3813d6c76676806
andrewmbacon <andrew.bacon@me.com>
2015-05-18 20:01:16 -0400

    Finished Subscribe page

--- page-subscribe.php
+++ page-subscribe.php
@@ -20,57 +20,53 @@ get_header();
 			</header><!-- .entry-header -->
 		
 			<div class="entry-content">
-				<?php the_content(); ?>
-				<?php
-					wp_link_pages( array(
-						'before' => '<div class="page-links">' . __( 'Pages:', 'commonplace' ),
-						'after'  => '</div>',
-					) );
-				?>
+				<div class="row">
+					<div class="col-sm-4">
+						<?php the_content(); ?>
+						<?php
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . __( 'Pages:', 'commonplace' ),
+								'after'  => '</div>',
+							) );
+						?>
+					</div>
+					<div class="col-sm-8">
+						<h2>RSS Feeds</h2>  
+						<ul>
+							<li><a href="<?php get_bloginfo(url); ?>/feed/">Common-place Master Feed</a> All articles, as soon as they're published.</li>
+						</ul>
+						<div class="row">
+							<div class="col-sm-6">
+								<h3>Topic Feeds</h3>
+								<ul>
+								<?php 
+								$topics = get_terms( 'topic');
+								foreach ($topics as $topic) {
+									echo '<li><a href="'.get_bloginfo(url) .'/topic/'.$topic->slug.'/feed">'.$topic->name.'</a></li>';
+								}
+								?>
+								</ul>
+							</div>
+							<div class="col-sm-6">
+								<h3>Column Feeds</h3>
+								<ul>
+								<?php 
+								$columns = get_terms( 'column');
+								foreach ($columns as $column) {
+									echo '<li><a href="'.get_bloginfo(url) .'/column/'.$column->slug.'/feed">'.$column->name.'</a></li>';
+								}
+								?>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div><!-- .entry-content -->
 		</article><!-- #post-## -->
 	<?php endwhile; // end of the loop. ?>
 	</div>
 </div>
-<div class="row">
-	<div class="col-sm-4">
-		<h2>Email Subs</h2>
-		<h2>Social Media</h2>
-		<?php 
-			if (wp_get_nav_menu_object('Social Media')){
-				$defaults = array(
-					'menu'			=> 'Social Media',
-					'container'       => false,
-					'items_wrap'      => '<ul id="%1$s" class="%2$s nav nav-pills">%3$s</ul>',
-					'depth'			=> 1,
-					'fallback_cb'		=> false
-				);
-				wp_nav_menu( $defaults );
-			}
-		?>
-	</div>
-	<div class="col-sm-8">
-		<h2>RSS Feeds</h2>
-		<p>Master Feed</p>
-		<h3>Topic Feeds</h3>
-		<ul>
-		<?php 
-		$topics = get_terms( 'topic');
-		foreach ($topics as $topic) {
-			echo '<li><a href="'.get_bloginfo(url) .'/'.$topic->slug.'">'.$topic->name.'</a></li>';
-		}
-		?>
-		</ul>
-		<h3>Column Feeds</h3>
-		<?php 
-		$columns = get_terms( 'column');
-		foreach ($columns as $column) {
-			echo $column->name;
-			echo $column->slug;
-		}
-		?>
-	</div>
-</div>
+
 
 <?php 
 get_footer();
